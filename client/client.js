e6dce2c2f23e720762cd82737a52ce130f129a92
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-03-31 17:12:58 -0400

    Add files via upload

--- client/client.js
+++ client/client.js
@@ -1,1 +1,32 @@
-test
+const { Client } = require('m2m');
+
+let client = new Client();
+
+client.connect(() => {
+
+  // test payload
+  //let payload = {type:'random'};
+
+  //client.sendData({id:500, channel:'ipc-channel', payload:payload}, (data) => {
+  client.watch({id:500, channel:'ipc-channel'}, (data) => {  
+    try{
+      let jd = JSON.parse(data);
+      console.log('rcvd json data:', jd);
+    }
+    catch (e){
+      console.log('rcvd string data:', data);
+    }
+  });
+
+  //client.sendData({id:600, channel:'ipc-channel', payload:payload}, (data) => {
+  client.watch({id:600, channel:'ipc-channel'}, (data) => {  
+    try{
+      let jd = JSON.parse(data);
+      console.log('rcvd json data:', jd);
+    }
+    catch (e){
+      console.log('rcvd string data:', data);
+    }
+  });
+
+});
