798d173507fa6e9947d9aaed233d371991e2b78f
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-04-04 11:43:14 -0400

    Update client.js

--- client/client.js
+++ client/client.js
@@ -4,22 +4,7 @@ let client = new Client();
 
 client.connect(() => {
 
-  // test payload
-  //let payload = {type:'random'};
-
-  //client.sendData({id:500, channel:'ipc-channel', payload:payload}, (data) => {
-  client.watch({id:500, channel:'ipc-channel'}, (data) => {  
-    try{
-      let jd = JSON.parse(data);
-      console.log('rcvd json data:', jd);
-    }
-    catch (e){
-      console.log('rcvd string data:', data);
-    }
-  });
-
-  //client.sendData({id:600, channel:'ipc-channel', payload:payload}, (data) => {
-  client.watch({id:600, channel:'ipc-channel'}, (data) => {  
+  client.watch({id:400, channel:'ipc-channel'}, (data) => {  
     try{
       let jd = JSON.parse(data);
       console.log('rcvd json data:', jd);
